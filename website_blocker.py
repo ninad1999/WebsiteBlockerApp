945cdb33302373ea8e9c946f7bb63e7949434fc3
Ninad Sakhardande <ninad.sakhardande@gmail.com>
2018-05-26 11:08:56 +0530

    V1

---
+++ website_blocker.py
@@ -0,0 +1,30 @@
+import time
+from datetime import datetime as dt
+
+host_path = "/etc/hosts"
+redirect = "127.0.0.1"
+
+website_list = ["facebook.com", "wwww.facebook.com", "www.youtube.com", "youtube.com"]
+
+while True:
+
+	if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16) :
+		print("WORKING HOURS")
+		with open(host_path, 'r+') as file:
+			contents = file.read()
+			for website in website_list:
+				if website in contents:
+					pass
+				else :
+					file.write(redirect + " " + website + '\n')
+	else :
+		with open(host_path, "r+") as file:
+			contents = file.readlines()
+			file.seek(0) # file.seek(0)  points to the first character of the file while file.seek() will point to the last character before appending new content
+			for line in contents: # hence the loop appends the new content before the existing content
+				if not any(website in line for website in website_list) :
+					file.write(line)
+			file.truncate() # removes everything from the file after the appending
+		print("Fun Hours")
+	
+	time.sleep(5)
